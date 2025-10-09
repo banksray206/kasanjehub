065dd37bb6b0f6c3891c6f688ecb12f7d3a23b24
banksray206 <banksray206@gmail.com>
2025-10-09 15:30:30 +0300

    e25

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("font-body antialiased")}>
+      <body className="font-body antialiased">
         <AuthProvider>
           <CartProvider>
             <div className="pb-20 md:pb-0">
